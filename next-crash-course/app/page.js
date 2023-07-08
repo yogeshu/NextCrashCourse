f9b0cc9e91b22a2befc477877e38ddef39f2a4de
Yogeshu <yogeshbhavsar1994@gmail.com>
2023-07-08 11:51:26 +0530

    Added Routes like About And Teams, as well as Added Components Like Header for full application

--- next-crash-course/app/page.js
+++ next-crash-course/app/page.js
@@ -1,7 +1,5 @@
+import Link from "next/link";
+
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-6xl font-bold text-center"> Next.js Crash Course</h1>
-    </main>
-  );
+  return <>Welcome To My NextJs CrashCourse</>;
 }
